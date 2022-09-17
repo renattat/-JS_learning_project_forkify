c784fbcc6c085efce967f9e415cc31ed6cc025d3
Сямиуллин Ренат Алиевич <RSyamiullin@inno.local>
2022-09-17 11:37:51 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log(`Welcome to the application!`);
+};
+
 const init = function () {
   bookmarksView.addHandlerRennder(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -132,6 +136,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(conrolPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
