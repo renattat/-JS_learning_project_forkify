d7472f4f6583873058be1c20b94e59d32cd428f5
Сямиуллин Ренат Алиевич <RSyamiullin@inno.local>
2022-09-17 11:27:32 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a0a17927-de7a-4213-b625-47fb89719f2a';
+export const MODAL_CLOSE_SEC = 2.5;
